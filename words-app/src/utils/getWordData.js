6284bb593ae9c69c78b592fe8a7746d3e1fb709c
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-09-21 17:00:15 +0300

    fetch data related to #3

---
+++ words-app/src/utils/getWordData.js
@@ -0,0 +1,7 @@
+const getWordData = (word) => {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+export default getWordData;
